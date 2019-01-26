8c4a41e9bba913231b2c2b1e1f98c27c6633740f
Raghunandhan0125 <42947417+Raghunandhan0125@users.noreply.github.com>
2019-01-25 19:18:21 -0500

    Update ShowFileContents.java

--- src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
+++ src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
@@ -5,17 +5,21 @@ import java.io.PrintWriter;
 
 public class ShowFileContents {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+        /* checking for the commandline argument */
         if (args.length != 1) {
             System.out.println("Usage: java ShowFileContents <directory>");
+	     return;
         }
         String dirname = args[0];
-
+	
         File dir = new File(dirname);
+        /* check if the directory exist or not */
         if (!dir.exists()) {
             System.err.println(dirname + " does not exist");
             return;
         }
+	/* check if it is a valid directory or not */
         if (!dir.isDirectory()) {
             System.err.println(dirname + " is not a directory");
             return;
@@ -23,9 +27,14 @@ public class ShowFileContents {
 
         PrintWriter outWriter = new PrintWriter(System.out);
         for (File file: dir.listFiles()) {
+		/* for each file in the direcotry if it is a directory skip it
+		   otherwise send the path of the file to print the contents
+		*/
         	if (file.isDirectory()) continue;
         	FileStringifier fileStringifier = new SlowFileStringifier(file.getPath());
         	fileStringifier.display(outWriter);
         }
     }
   }
+
+}
