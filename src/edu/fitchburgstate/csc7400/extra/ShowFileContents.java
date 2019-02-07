399fae572930a2fd45ce768da15e625564aed776
Raghunandhan0125 <42947417+Raghunandhan0125@users.noreply.github.com>
2019-02-06 22:29:32 -0500

    updated changes

--- src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
+++ src/edu/fitchburgstate/csc7400/extra/ShowFileContents.java
@@ -1,17 +1,36 @@
+/*
+ * Created on : 
+ * Author : 
+ */
 package edu.fitchburgstate.csc7400.extra;
 
 import java.io.File;
 import java.io.PrintWriter;
 
+
+/**
+ * The Class ShowFileContents. In this the given path as argument to main method is validated
+ * and an object of slowFileStringifier is created and display method is called using the slowFileStringifier object reference.
+ */
 public class ShowFileContents {
 
+/**
+ * The main method.
+ *
+ * @param args the String array arguments of main holds the path of text files as value.
+ */
+//	Directory path to be given as arguments to main methods String array(command line arguments).
     public static void main(String[] args) {
+
+//    	If argument to main method is not passed then thread enters inside if block.
         if (args.length != 1) {
             System.out.println("Usage: java ShowFileContents <directory>");
         }
+//       Path argument is stored in dirname 
         String dirname = args[0];
-
+//        File object is created and path is passed as an argument to file object constructor.
         File dir = new File(dirname);
+//        If the given directory doesn't exist then thread enters into this if block and execution stops.
         if (!dir.exists()) {
             System.err.println(dirname + " does not exist");
             return;
@@ -20,11 +39,15 @@ public class ShowFileContents {
             System.err.println(dirname + " is not a directory");
             return;
         }
-
+//		PrintWriter object is created and System.out is sent as an argument to Printwriter object's constructor.
         PrintWriter outWriter = new PrintWriter(System.out);
+//       For each loop is used to list out all the files one by one. 
         for (File file: dir.listFiles()) {
         	if (file.isDirectory()) continue;
+//        	Object of SlowFileStringifier is created and is handled by FileStringifier interface as SlowFileStringifier is implementing FileStringifier.
+//        	File path is passed to this object constructor by making use of getPath() method.
         	FileStringifier fileStringifier = new SlowFileStringifier(file.getPath());
+//        	A non static method Display inside SlowStringifier implimented from FileStringifier is called using FileStringifier object reference.
         	fileStringifier.display(outWriter);
         }
     }
--- src/edu/fitchburgstate/csc7400/extra/SlowFileStringifier.java
+++ src/edu/fitchburgstate/csc7400/extra/SlowFileStringifier.java
@@ -1,34 +1,71 @@
+/*
+ * Created on: 
+ * Author : 
+ * 
+ */
 package edu.fitchburgstate.csc7400.extra;
 
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * The Class SlowFileStringifier.
+ */
+//SlowFileStringifier implementing FileStringifier interface.
 public class SlowFileStringifier implements FileStringifier {
 
+/**
+ * Instantiates a new slow file stringifier.
+ *
+ * @param filename holds the value of file path
+ */
+//	Constructor of SlowFileStringifier class and gets executed when ever object for this class is created.
     public SlowFileStringifier(String filename) {
         this.fileName = filename;
     }
 
+
+/*
+ * @see edu.fitchburgstate.csc7400.extra.FileStringifier#display(java.io.PrintWriter)
+ */
+//Interface method being implemented.
     public void display(PrintWriter out) {
+//    	Println() method is called out on System.out reference hence it prints output on console and stringify() is called inside this method.
     	out.println(this.stringify());
+//    	flush() method is used to flush out any data that got stuck or saved inside the output stream onto the console.
     	out.flush();
     }
     
+
+/*
+ * @see edu.fitchburgstate.csc7400.extra.FileStringifier#stringify()
+ */
+//    This stringify() method is used to return the file contents.
     public String stringify() {
+//    	Local FileReader reference.
         FileReader f;
 		try {
+//			FileReader object created with filepath thats passed to the constructor.
 			f = new FileReader(this.fileName);
+//			BufferedReader object is created and it takes Filereader object reference as it can't directly communicate with the file.
 	        BufferedReader bf = new BufferedReader(f);
-	
+//			ArrayList collection object of generic String.
 	        ArrayList<String> lines = new ArrayList<String>();
+//	        String type local variable line
 	        String line = null;
+//	        while loop used to read the contents in files line by line using readLine() method.
 	        while ((line = bf.readLine()) != null) {
+//	        	Thread.sleep() stops the execution of thread for given amount of time.
 	            Thread.sleep(2000);
+//	            add() method is used to add the each read line to ArrayList collection object by being called with ArrayList object reference.
 	            lines.add(line);
 	        }
+//	        close() method is used to close the costly resource BufferedReader.
 	        bf.close();
+//	        stringify() method returning the ArrayList object reference.
 	        return String.join("\n", lines);
-        
+	        
+//        Here catch blocks will handle the exceptions that rise during execution of code.
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			System.err.println("File "+this.fileName+" was not found.");
@@ -42,5 +79,7 @@ public class SlowFileStringifier implements FileStringifier {
 		return "";
     }
 
+/** The file name. */
+//A private String variable and this can only be used in this class.
     private final String fileName;
 }
